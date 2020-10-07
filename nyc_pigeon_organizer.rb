8e029a009919d7a858c761f31a3181b6dd22a007
Luis Torres <luistjr@gmail.com>
2020-10-07 20:45:28 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
+# write your code here!
+  
+  #create a hash where pigeon names are the keys, with each name pointing to a hash of their attributes
+  
+  #collecting each pigeon by name and insert it as the key of a new hash 
+  
+  #keys should have :color, :gender, and :lives
+  
+  #first step - make theo, peter jr, and lucky keys 
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  final_results = data.each_with_object({}) do |(key, value), final_array| 
+    # binding.pry
+    value.each do |inner_key, names|
+    names.each do |name|
+      if !final_array[name]
+        final_array[name] = {}
+      end 
+      if !final_array[name][key]
+          !final_array[name][key] = [] 
+      end 
+      final_array[name][key].push(inner_key.to_s)
+    end
+  end 
+end  
+  # binding.pry 
 end
