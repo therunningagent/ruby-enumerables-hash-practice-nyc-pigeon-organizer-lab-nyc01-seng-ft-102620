91449f2b024a2f4622b3329f153ca1a94f35ef3f
Luis Torres <luistjr@gmail.com>
2020-10-07 20:37:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -36,7 +36,9 @@ def nyc_pigeon_organizer(data)
     names.each do |name|
       binding.pry
       if !final_array[name]
-        
+        final_array[name] = {}
+      end 
+      
     end
   end 
     final_array
